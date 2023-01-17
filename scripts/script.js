3180bb924843fa0774b6bfcb7a953cbc92fe1b0a
Gage-Fleming <gage.fleming@outlook.com>
2023-01-16 23:36:22 -0700

    Fight with timeline

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,2 @@
-window.addEventListener('scroll', function() {
-    var fadeInElements = document.getElementsByClassName('fadeInElement');
 
-    for (var i = 0; i < fadeInElements.length; i++) {
-      var element = fadeInElements[i];
-      var positionFromTop = fadeInElements[i].getBoundingClientRect().top;
-      if (positionFromTop - window.innerHeight <= -500) {
-        element.classList.remove('fadeInElement');
-        element.classList.add('fadeInElementVisible');
-      } else {
-        element.style.opacity = 1;
-      }
-    }
-  });
   
