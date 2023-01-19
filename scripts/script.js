871c64049167c95168650753a6d2b657ee1e548f
Gage-Fleming <gage.fleming@outlook.com>
2023-01-18 22:39:09 -0700

    Get intersection obsever working

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,19 @@
+/* Create intersection observer for timeline */
 
+let timelineArticles = document.querySelectorAll('.timelineArticle');
+
+let observer = new IntersectionObserver(
+    entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    })
+    },
+    {
+        threshold: .9,
+    }
+);
+
+timelineArticles.forEach(timelineArticle => {
+    observer.observe(timelineArticle);
+});
   
