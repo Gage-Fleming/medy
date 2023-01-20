71babcaefdb8fa53c2fb583a875a9c66d9003ba5
Gage-Fleming <gage.fleming@outlook.com>
2023-01-19 23:03:32 -0700

    Allow JS to apply alt to updated images

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 
 // thich-nhat-hanh.html JS
+/**
+ * Please note the intersection obsever code was inspired by the below video.
+ * https://www.youtube.com/watch?v=2IbRtjez6ag
+ */
 
 // Store variable references to timeArticle class elements and the time image. These
 // Are to be manipulated by the intersection observer and setInterval
@@ -43,24 +47,31 @@ function checkShow() {
             switch (i) {
                 case 0:
                     timelineImage.src="../images/thich-nhat-hanh-images/1950s.jpg";
+                    timelineImage.alt="Thich Nhat Hanh and 5 other monks pose for a photo";
                     break;
                 case 1:
                     timelineImage.src="../images/thich-nhat-hanh-images/1962.jpg";
+                    timelineImage.alt="Thich Nhat Hanh in the midts of teaching a class full of children";
                     break;
                 case 2:
                     timelineImage.src="../images/thich-nhat-hanh-images/1966.jpg";
+                    timelineImage.alt="Young monks posing for the camera in traditional monk clothing";
                     break;
                 case 3:
                     timelineImage.src="../images/thich-nhat-hanh-images/1967.jpg";
+                    timelineImage.alt="Martin Luther King and Thich Nhat Hanh listing to a conversation";
                     break;
                 case 4:
                     timelineImage.src="../images/thich-nhat-hanh-images/1982.jpg";
+                    timelineImage.alt="A forest pathway with monks in the midts of a communcal meditation";
                     break;
                 case 5:
                     timelineImage.src="../images/thich-nhat-hanh-images/2014.jpg";
+                    timelineImage.alt="An aged Thich Nhat Hanh poses for a photo";
                     break;
                 case 6:
                     timelineImage.src="../images/thich-nhat-hanh-images/2022.jpg";
+                    timelineImage.alt="Thich Nhat Hanh pictured meditating on his poems";
                     break;
                 default:
             }
