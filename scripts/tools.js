85c9bcd3a5ab0c9465e02feb77744282fa14a95c
Gage-Fleming <gage.fleming@outlook.com>
2023-01-22 20:17:38 -0700

    Timer is working

--- scripts/tools.js
+++ scripts/tools.js
@@ -9,7 +9,7 @@ tools.html Code
 
 // Declare the variables to references to all the elements in the user form
 let meditationFormSubmit = document.getElementById('meditationFormSubmit');
-let meditationFormStop = document.getElementById('meditationFormStop');
+let meditationFormPause = document.getElementById('meditationFormPause');
 
 let soundTypeOptions = document.querySelectorAll('input[name="sounds"]');
 let durationSelector = document.getElementById('duration');
@@ -17,6 +17,17 @@ let intervalBellSelector = document.getElementById('intervalBell');
 let bellVolumeSelector = document.getElementById('sound');
 
 let outputTime = document.getElementById('outputTime');
+
+// Create timer interval variable
+let timerInterval;
+
+// Create variable to store if interval is running or not.
+let intervalOn = false;
+
+// Create variables to store the time that has passed since the select button has been clicked.
+// And a variable to store the timeleft when subtracting timePassed from timeLeft
+let timePassed;
+let timeLeft;
 
 // Decalre variables that will hold the value of the elements in the user form
 // Any time variable is to be stored in seconds
@@ -36,6 +47,7 @@ let volume;
  * 
  */
 function formatTime(time) {
+
     // Retrieve the largest round number when dividing the input time in seconds to get the current minute
     let minutes = String(Math.floor(time / 60));
 
@@ -51,16 +63,28 @@ function formatTime(time) {
     return `${minutes}:${seconds}`
 }
 
-function startTimer(timePassed, timeLeft) {
+function setTimer() {
+    // Create timer interval to count the clock down.
     timerInterval = setInterval(() => {
-        timePassed = timePassed += 1;
-        timeLeft = duration - timePassed;
+    timePassed = timePassed += 1;
+    timeLeft = duration - timePassed;
 
-        outputTime.innerHTML = formatTime(timeLeft);
+    outputTime.innerHTML = formatTime(timeLeft);
     }, 1000);
+
+    intervalOn = true;
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    intervalOn = false;
 }
 
 meditationFormSubmit.onclick = function() {
+    // Ensure Interval is cleared at start.
+    clearInterval(timerInterval);
+    intervalOn = false;
+
     // Get the sound type the user has selected from radio buttons
     for (let i = 0; i < soundTypeOptions.length; i++) {
         if (soundTypeOptions[i].checked) {
@@ -72,10 +96,8 @@ meditationFormSubmit.onclick = function() {
     // Retrieve the selected duration and convert it to seconds
     duration = Number(durationSelector.value.slice(0, 3)) * 60;
 
-    // Create variables to store the time that has passed since the select button has been clicked.
-    // And a variable to store the timeleft when subtracting timePassed from timeLeft
-    let timePassed = 0;
-    let timeLeft = duration;
+    timePassed = 0;
+    timeLeft = duration;
 
     // Update outputTime to display chosen time
     outputTime.innerHTML = formatTime(timeLeft);
@@ -91,7 +113,21 @@ meditationFormSubmit.onclick = function() {
     // Get volume user has selected
     volume = bellVolumeSelector.value;
 
-    startTimer(timePassed, timeLeft);
+    setTimer();
+};
+
+meditationFormPause.onclick = function() {
+    if (intervalOn && meditationFormPause.innerHTML == 'Pause') {
+        stopTimer();
+        outputTime.innerHTML = 'Paused';
+        meditationFormPause.innerHTML = 'Continue'
+        intervalOn = false;
+    } else if (!intervalOn && meditationFormPause.innerHTML == 'Continue') {
+        outputTime.innerHTML = formatTime(timeLeft);
+        setTimer();
+        meditationFormPause.innerHTML = 'Pause'
+        intervalOn = true;
+    }
 };
 /* ==========================================================================
 Section comment block
