bedd4d5130930522970accc3b55e64f1bffbdb07
Gage-Fleming <gage.fleming@outlook.com>
2023-01-26 23:40:32 -0700

    Fix script reference and work on js for search tool

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,6 @@
+/* ==========================================================================
+search tool code
+========================================================================== */
 
 // Get a reference to all standard articles
 let standArticles = document.querySelectorAll('.standardArticle');
@@ -5,7 +8,8 @@ let standArticles = document.querySelectorAll('.standardArticle');
 // Get a reference to all timeline articles
 let timeArticles = document.querySelectorAll('.timelineArticle');
 
-console.log(timeArticles[0]);
+// Get a reference to all tools
+let tools = document.querySelectorAll('.tool');
 
 // Create an article array to hold data for searchable items
 let searchArray = [];
@@ -14,7 +18,7 @@ let searchArray = [];
  * Loop through all elements that have the class standard article and store the text in these articles in an array of objects. Each object will represent the article
  * in question. The information on the object properties can be found below.
  * 
- * articleNumber: stores the number of the article {int}
+ * articleNumber: stores the number of the article {Int}
  * author: Stores the author name {String}
  * date: Stores the article date {String}
  * heading: Stores the text in the heading of the article {String}
@@ -36,15 +40,14 @@ for (let i = 0; i < standArticles.length; i++) {
 }
 
 /**
- * Loop through all elements that have the class standard article and store the text in these articles in an array of objects. Each object will represent the article
+ * Loop through all elements that have the class timeline article and store the text in these articles in an array of objects. Each object will represent the article
  * in question. The information on the object properties can be found below.
  * 
- * articleNumber: stores the number of the article {int}
- * author: Stores the author name {String}
- * date: Stores the article date {String}
+ * articleNumber: stores the number of the article {Int}
  * heading: Stores the text in the heading of the article {String}
  * textBlurb: Stores the text in the body of the article {String}
  * type: Used to help javascript decide how to display the return result {String}
+ * year: Stores the year of the current timeline article {Int}
  */
 for (let i = 0; i < timeArticles.length; i++) {
     let currentArticleInfo = timeArticles[i];
@@ -52,11 +55,28 @@ for (let i = 0; i < timeArticles.length; i++) {
         {
             articleNumber: i,
             heading: currentArticleInfo.children[2].children[0].innerHTML,
-            year: currentArticleInfo.children[0].children[0].innerHTML,
             textBlurb: currentArticleInfo.children[2].children[1].innerHTML,
             type: 'timeline',
+            year: Number(currentArticleInfo.children[0].children[0].innerHTML),
         }
     );
 }
 
-console.log(searchArray);
+/**
+ * Loop through all elements that have the class tool and store the header in these tools in an array of objects. Each object will represent the article
+ * in question. The information on the object properties can be found below.
+ * 
+ * heading: Stores the text in the heading of the article {String}
+ * toolNumber: stores the number of the article {Int}
+ * type: Used to help javascript decide how to display the return result {String}
+ */
+for (let i = 0; i < tools.length; i++) {
+    let currentToolInfo = tools[i];
+    searchArray.push(
+        {
+            heading: currentToolInfo.children[0].children[0].innerHTML,
+            toolNumber: i,
+            type: 'tool',
+        }
+    );
+}
