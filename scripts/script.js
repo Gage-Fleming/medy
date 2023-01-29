2c3789cb63babc549dc337f423deb68a36ae8f20
Gage-Fleming <gage.fleming@outlook.com>
2023-01-29 15:42:58 -0700

    Finish meditations page

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,15 @@ let donationButton = document.getElementById('donate');
 donationButton.onclick = () => {
     window.alert('Thank you for your donation!');
 };
+
+let buyBook = document.getElementById('buyBook');
+
+buyBook.onclick = () => {
+    window.alert('Thank you for your purchase!');
+};
+
+let calmSubscribe = document.getElementById('calmSubscribe');
+
+calmSubscribe.onclick = () => {
+    window.alert('Thank you for your subscription!');
+};
