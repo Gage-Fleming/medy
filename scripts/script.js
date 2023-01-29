54f17f2ba104f0d5ef35dec68ceae7dc8329f847
Gage-Fleming <gage.fleming@outlook.com>
2023-01-29 16:02:36 -0700

    Work on meditation page

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,24 @@ Basic scripts JS
 /**
  * Create event listener for when donation button is clicked
  */
-
 let donationButton = document.getElementById('donate');
 
 donationButton.onclick = () => {
     window.alert('Thank you for your donation!');
 };
 
+/**
+ * Create event listener for when book buy button is clicked
+ */
 let buyBook = document.getElementById('buyBook');
 
 buyBook.onclick = () => {
     window.alert('Thank you for your purchase!');
 };
 
+/**
+ * Create event listener for when calm button is clicked
+ */
 let calmSubscribe = document.getElementById('calmSubscribe');
 
 calmSubscribe.onclick = () => {
