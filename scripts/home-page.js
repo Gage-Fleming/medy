eba5c41f47d5a1f8e5e7585c65cedd797eaf686a
Gage-Fleming <gage.fleming@outlook.com>
2023-01-29 22:06:19 -0700

    Finish the poll for home page

---
+++ scripts/home-page.js
@@ -0,0 +1,78 @@
+/* Poll code    
+========================================================================== */
+
+// Get reference to all poll buttons
+let pollButtons = document.querySelectorAll('.pollButton');
+
+// Create variables to hold reference to all poll buttons
+let answerOne = pollButtons[0];
+let answerTwo = pollButtons[1];
+let answerThree = pollButtons[2];
+let answerFour = pollButtons[3];
+
+// Create variables to hold fake statistics for all answer in poll.
+let answerOneCount = 30;
+let answerTwoCount = 25;
+let answerThreeCount = 20;
+let answerFourCount = 10;
+
+let totalVotes;
+
+function getTotalVotes() {
+    totalVotes = answerOneCount + answerTwoCount + answerThreeCount + answerFourCount;
+}
+
+function showPoll() {
+    for (let i = 0; i < pollButtons.length; i++) {
+        let currentButton = pollButtons[i]
+        let currentButtonParent = currentButton.parentElement;
+        currentButton.classList.add('pollButtonHide');
+    
+        switch (i) {
+            case 0:
+                currentButtonParent.classList.add('pollResponseBacking');
+                currentButtonParent.children[1].style.width = `${100*(answerOneCount / totalVotes)}%`;
+                currentButtonParent.children[1].innerHTML = `${Math.round(100*(answerOneCount / totalVotes))}%`;
+                break;
+            case 1:
+                currentButtonParent.classList.add('pollResponseBacking');
+                currentButtonParent.children[1].style.width = `${100*(answerTwoCount / totalVotes)}%`;
+                currentButtonParent.children[1].innerHTML = `${Math.round(100*(answerTwoCount / totalVotes))}%`;
+                break;
+            case 2:
+                currentButtonParent.classList.add('pollResponseBacking');
+                currentButtonParent.children[1].style.width = `${100*(answerThreeCount / totalVotes)}%`;
+                currentButtonParent.children[1].innerHTML = `${Math.round(100*(answerThreeCount / totalVotes))}%`;
+                break;
+            case 3:
+                currentButtonParent.classList.add('pollResponseBacking');
+                currentButtonParent.children[1].style.width = `${100*(answerFourCount / totalVotes)}%`;
+                currentButtonParent.children[1].innerHTML = `${Math.round(100*(answerFourCount / totalVotes))}%`;
+                break;
+        }
+    }
+}
+
+answerOne.onclick = function () {
+    answerOneCount++;
+    getTotalVotes()
+    showPoll();
+}
+
+answerTwo.onclick = function () {
+    answerTwoCount++;
+    getTotalVotes()
+    showPoll();
+}
+
+answerThree.onclick = function () {
+    answerThreeCount++
+    getTotalVotes()
+    showPoll();
+}
+
+answerFour.onclick = function () {
+    answerFourCount++;
+    getTotalVotes()
+    showPoll();
+}
